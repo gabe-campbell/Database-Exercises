2d1abf9e9c5d8ebb002694dff2e6c01fc28fa78e
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-03-14 15:25:03 -0500

    albums_seeder.sql commit

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist, name, release_date, genre, sales_in_millions)
+VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, rock, R&B', 48.1)
+,('AC/DC', 'Back in Black', 1980, 'Hard rock', 25.9)
+,('Whitney Houston / Various artists', 'The Bodyguard', 1992, 'Soundtrack/R&B, soul, pop', 22.7)
+,('Meat Loaf', 'Bat Out o Hell', 1977, 'Hard rock, progressive rock', 27.4)
+,('Eagles', 'Their Greatest Hits', 1976, 'Rock, soft rock, folk rock', 20.6)
+,('Bee Gees / Various artists', 'Saturday Night Fever', 1977, 'Disco', 19)
+,('Fleetwood Mac', 'Rumours', 1977, 'Soft rock', 27.9);
